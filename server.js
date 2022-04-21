6421ec6c8e37a6a3dfe36a0242642c9e644290f4
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-04-21 07:36:57 -0700

    updating edits made in replit

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+var multer  = require('multer');
+var upload = multer(); // file kept in memory w/options omitted
 require('dotenv').config()
 
 var app = express();
@@ -8,11 +10,19 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+// take uploaded file (name: upfile) and respond with name, type, and size (bytes)
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json(
+    {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    }
+  );
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
